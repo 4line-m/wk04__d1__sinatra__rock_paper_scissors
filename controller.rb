943b515483a533c170642a3e4be9677528e5d0aa
Aline Mokfa <alinemokfa@hotmail.co.uk>
2017-10-16 22:16:35 +0100

    added ERB for results

--- controller.rb
+++ controller.rb
@@ -5,3 +5,9 @@ require_relative('./models/game.rb')
 get '/' do
   erb(:home)
 end
+
+get '/throw/:player_1/:player_2' do
+  game = Game.new(params[:player_1], params[:player_2])
+  @game = game.throw()
+  erb(:result)
+end
